9b02f78c001bd805dab1cbdb0d4c103dda7b6315
Arden Rasmussen <ardenisthebest@gmail.com>
2019-08-20 16:20:01 -0700

    Added luajit and sol3 dependencies

---
+++ resources/test.lua
@@ -0,0 +1,2 @@
+print("Hello World!")
+print(math.cos(3.1415));
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
-int main(int argc, char *argv[])
-{
-  
+#include <sol/sol.hpp>
+
+int main(int argc, char *argv[]) {
+  sol::state lua;
+  lua.open_libraries(sol::lib::base, sol::lib::math);
+  lua.script_file("resources/test.lua");
   return 0;
 }
