14c311664dec05f528a0dead6c9995097440dde6
Arden Rasmussen <ardenisthebest@gmail.com>
2019-11-16 21:07:44 -0800

    Minor changes

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -144,6 +144,10 @@ glm::vec3 specula::renderer::ray_march(
   if (obj == nullptr)
     return glm::vec3(0.0f, 0.0f, 0.0f);
 
+  if (depth == 1) {
+    LDEBUG("HIT {}: {} @ {}", depth, obj, t);
+  }
+
   glm::vec3 p = o + t * d;
   glm::vec3 normal = obj->normal(p, ep);
 
@@ -161,18 +165,19 @@ glm::vec3 specula::renderer::ray_march(
     dir.x = glm::dot(glm::vec3(rotx.x, roty.x, normal.x), sampled_dir);
     dir.y = glm::dot(glm::vec3(rotx.y, roty.y, normal.y), sampled_dir);
     dir.z = glm::dot(glm::vec3(rotx.z, roty.z, normal.z), sampled_dir);
-    if (depth == 0) {
-      LDEBUG("P: {},{},{} N: {},{},{} DIR: {},{},{}", p.x, p.y, p.z, normal.x,
-             normal.y, normal.z, dir.x, dir.y, dir.z);
-    }
     float cost = glm::dot(dir, normal);
     clr += rr_factor *
-           (ray_march(p + 10.0f * ep * dir, dir, objs, ep, t_max, depth + 1) *
+           (ray_march(p + 2.0f * ep * dir, dir, objs, ep, t_max, depth + 1) *
             obj->mat_->base_color) *
            cost * 0.1f;
   } else if (obj->mat_->type == material::Type::SPECULAR) {
     float cost = glm::dot(d, normal);
     glm::vec3 dir = glm::normalize(d - normal * (cost * 2.0f));
+    if (depth == 0) {
+      LDEBUG("O:{},{},{} D: {},{},{} -> P: {},{},{} N: {},{},{} DIR: {},{},{}",
+             o.x, o.y, o.z, d.x, d.y, d.z, p.x, p.y, p.z, normal.x, normal.y,
+             normal.z, dir.x, dir.y, dir.z);
+    }
     clr += rr_factor *
            ray_march(p + 2.0f * ep * dir, dir, objs, ep, t_max, depth + 1);
   } else if (obj->mat_->type == material::Type::REFRACTIVE) {
