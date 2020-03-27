af9f441e3d7c8dfc999d425148c407d9a0490a73
Arden Rasmussen <ardenisthebest@gmail.com>
2020-03-26 20:13:02 -0700

    Began implementing spectrum

--- lib/src/core/spectrum/common.cpp
+++ lib/src/core/spectrum/common.cpp
@@ -20,7 +20,39 @@ void specula::sort_spectrum_samples(Float *lambda, Float *vals, std::size_t n) {
   }
 }
 
-specula::Float specula::average_spectrum_samples(const Float *lambda, const Float *vals,
-                               std::size_t n, Float lambda_start,
-                               Float lambda_end){}
+specula::Float specula::average_spectrum_samples(const Float *lambda,
+                                                 const Float *vals,
+                                                 std::size_t n,
+                                                 Float lambda_start,
+                                                 Float lambda_end) {
+  for (std::size_t i = 0; i < n - 1; ++i)
+    CHECK_GT(lambda[i + 1], lambda[i]);
+  CHECK_LT(lambda_start, lambda_end);
+  if (lambda_end <= lambda[0])
+    return vals[0];
+  else if (lambda_start >= lambda[n - 1])
+    return vals[n - 1];
+  else if (n == 1)
+    return vals[0];
+  Float sum = 0;
+  if (lambda_start < lambda[0])
+    sum += vals[0] * (lambda[0] - lambda_start);
+  if (lambda_end > lambda[n - 1])
+    sum += vals[n - 1] * (lambda_end - lambda[n - 1]);
 
+  std::size_t i = 0;
+  while (lambda_start > lambda[i + 1])
+    ++i;
+  CHECK_LT(i + 1, n);
+  auto interp = [lambda, vals](Float w, std::size_t i) {
+    return lerp((w - lambda[i]) / (lambda[i + 1] - lambda[i]), vals[i],
+                vals[i + 1]);
+  };
+  for (; i + 1 < n && lambda_end >= lambda[i]; ++i) {
+    Float seg_lambda_start = std::max(lambda_start, lambda[i]);
+    Float seg_lambda_end = std::min(lambda_end, lambda[i + 1]);
+    sum += 0.5 * (interp(seg_lambda_start, i) + interp(seg_lambda_end, i)) *
+           (seg_lambda_end - seg_lambda_start);
+  }
+  return sum / (lambda_end - lambda_start);
+}
